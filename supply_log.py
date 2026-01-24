750e4a7b7077e899261e8e299a19963feb130cb6
Gowthaman M <gowthamanm585@gmail.com>
2026-01-24 12:18:12 +0530

    Intial commit of lab

---
+++ supply_log.py
@@ -0,0 +1,11 @@
+# Function Declaration
+def supply_log():
+    # try block to ensure log file presence
+    try:
+        with open('auth.log','r',buffering=8192) as file:
+            for log in file:
+                # passing only sshd logs include's corrupted sshd logs
+                if 'sshd[' in log:
+                    yield log
+    except FileNotFoundError:
+        return "File does not exist"
